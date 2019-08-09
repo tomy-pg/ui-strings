a77c163072249c29d58db3fad70709dca74bfa48
Tomy Reyes <tomy@peacegeeks.org>
2019-08-09 10:06:25 -0700

    Issue pg-irc/pathways-frontend#757 adjust strings.py for locales that do not have uppercase

--- bin/strings.py
+++ bin/strings.py
@@ -11,7 +11,7 @@ def main():
     explore_file = args.path
     content_list = place_file_content_in_list(explore_file)
     clean_list = clean_content_list(content_list)
-    new_content = replace_explore_file_content(clean_list)
+    new_content = adjust_explore_file_content(clean_list)
     new_explore_file = './explore/%s/explore-copy.po' % (args.target_language_code)
     write_content_to_new_file(new_explore_file, new_content)
 
@@ -40,15 +40,14 @@ def write_content_to_new_file(path, content):
         for line in content:
             file.write(line)
 
-def replace_explore_file_content(content_list):
+def adjust_explore_file_content(content_list):
     new_list = []
     for line in content_list:
         if re.match('msgid', line):
             content = replace_msgid_content(line)
             new_list.append(content)
         if re.match('msgstr', line):
-            content = replace_msgstr_content(line)
-            new_list.append(content)
+            new_list.append(line + '\n')
     return new_list
 
 def replace_msgid_content(content):
@@ -57,20 +56,10 @@ def replace_msgid_content(content):
     new_msgid_content = 'msgid' + new_msgid_content
     return new_msgid_content
 
-def replace_msgstr_content(content):
-    new_msgstr_content = remove_msgstr(content)
-    new_msgstr_content = replace_msg_value(new_msgstr_content)
-    new_msgstr_content = 'msgstr' + new_msgstr_content+'\n'
-    return new_msgstr_content
-
 def remove_msgid(text):
     text = re.sub(r'msgid', '', text)
     return text
 
-def remove_msgstr(text):
-    text = re.sub(r'msgstr', '', text)
-    return text
- 
 
 def replace_msg_value(text):
     return re.sub(r'([a-zA-Z]+)', turn_to_uppercase, text)
